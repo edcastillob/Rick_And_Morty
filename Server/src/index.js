9e99d70d48dbbf5e5707eab8929a5a2df57c2470
edcastillob <edwar.castillo@gmail.com>
2023-05-31 12:35:46 -0400

    act img

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,4 +1,4 @@
-const characters = require('./utils/data');
+const characters = require('./utils/data.js');
 var http = require("http");
 const PORT = 3001;
 
@@ -8,9 +8,9 @@ http.createServer((req, res) => {
     if(req.url.includes('/rickandmorty/character')){
        const id = req.url.split('/').pop();  
 
-       const character = characters.filter((character) => character.id === Number(id));
+       const character = characters.filter((character) => character.id === Number(id))[0];
        res.writeHead(200, { 'content-type':'application/json' });
-       res.end(JSON.stringify(character[0]))
+       res.end(JSON.stringify(character))
     }
 
  }).listen(PORT, 'localhost')
