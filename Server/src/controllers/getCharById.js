56a7b89d802331100d20ca49216e39d5e9f98f14
edcastillob <edwar.castillo@gmail.com>
2023-06-04 18:35:30 -0400

    getCharId

---
+++ Server/src/controllers/getCharById.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+
+
+const URL = 'https://rickandmortyapi.com/api/character/';
+
+const getCharById = (req, res) => { 
+	
+	
+		const { id } = req.params;
+		 axios(`${URL}${id}`)
+		.then(response => response.data)
+		.then(({status, name, species, origin, image, gender}) =>{
+				if(name){
+					const character= {
+						id, status, name, species, origin, image, gender
+					}
+					return res.status(200).json(character);
+				}
+				return res.status(404).send('Not found');
+		})
+		.catch(error => res.status(500).send(error.message))
+	}
+
+
+module.exports = { 
+	getCharById 
+};
+
+
+
+
--- Server/src/index.js
+++ Server/src/index.js
@@ -5,12 +5,8 @@ const PORT = 3001;
 
 
 
-
-
-
-
 server.listen(PORT, () => {
-   console.log('Server raised in port: ' + PORT);
+   console.log(`Server raised in port: ${PORT}`);
 });
 
 
