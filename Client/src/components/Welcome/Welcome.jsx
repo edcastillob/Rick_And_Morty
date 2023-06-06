69e2f7b07f4c4c988b0ecbdff39ffed94329880f
edcastillob <edwar.castillo@gmail.com>
2023-06-05 22:12:23 -0400

    Css

--- Client/src/components/Welcome/Welcome.jsx
+++ Client/src/components/Welcome/Welcome.jsx
@@ -1,5 +1,8 @@
 import welGif from '../../../public/images/wel.gif';
+import ec from '../../../public/ec.png';
+import henry from '../../../public/images/henry.png';
 import style from '../ModuleCss/Welcome.module.css';
+
 
 
 
@@ -12,6 +15,14 @@ export const Welcome = () => {
     </div>
     <h2 className={style.text}>Welcome to the Rick and Morty API navigate to use the features</h2>
 
+
+    <footer className={ style.footer }>
+        <img className={style.logoEdwar} src={ec} alt="Logo Edwar Castillo" />
+    <div className={ style.footerContainer}>
+        <p>© Ing. Edwar Castillo - Proyecto Integrador Henry.</p>
+    </div>
+        <img className={style.logoHenry} src={henry} alt="Logo Henry" />
+</footer>
     </>
   )
 }
